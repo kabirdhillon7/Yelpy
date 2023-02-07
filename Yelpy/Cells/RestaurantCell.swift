2d3f272a14965ff0bcbb09f8e4051e0ac2225e9e
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-07 15:29:00 -0800

    Initial commit

---
+++ Yelpy/Cells/RestaurantCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantCell.swift
+//  Yelpy
+//
+//  Created by Kabir Dhillon on 2/7/23.
+//  Copyright © 2023 memo. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
